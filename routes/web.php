f6c358f2c8ad0f6324e264839dab34f7901dc7c1
awal2645 <devboyawal@gmail.com>
2026-03-17 22:02:30 +0600

    fix:few problem

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
 use App\Http\Controllers\GoogleAuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Google OAuth callback (must be before catch-all)
 Route::get('/google/callback', [GoogleAuthController::class, 'callback'])->name('google.callback');
+
+// Password reset - required by Laravel's Password::sendResetLink notification
+// Redirects to SPA reset page with token & email as query params
+Route::get('/reset-password/{token}', function (Request $request, string $token) {
+    $email = $request->query('email');
+    $query = 'token=' . urlencode($token);
+    if ($email) {
+        $query .= '&email=' . urlencode($email);
+    }
+    return redirect('/reset-password?' . $query);
+})->name('password.reset');
 
 // Serve React app - root redirects to login
 Route::get('/', function () {
